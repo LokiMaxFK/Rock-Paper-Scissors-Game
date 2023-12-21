39cc04243b10e5fe0d078a49f5ca3a894186745e
Jordan <jordanacrga@gmail.com>
2023-12-21 17:38:45 -0600

    Se agregan y detectan cambios en los botones

--- js/game.js
+++ js/game.js
@@ -6,29 +6,68 @@ const types = {
     },
     scissors:{
         id: 2,
-        name: "rock",
-        beats: "papper"
+        name: "scissors",
+        beats: "paper"
     },
-    papper: {
+    paper: {
         id: 2,
-        name: "papper",
+        name: "paper",
         beats: "rock"
     }
-}
+};
+
+const options = ["rock", "scissors", "paper"];
 
 const board = document.querySelector(".board");
 const results = document.querySelector(".result");
+const optionPlayer = document.querySelector(".result__option--player");
+const optionHouse = document.querySelector(".result__option--house");
 
 board.addEventListener("click", (e)=>{
     console.log(e);
     if(e.target.matches(".option")){
         const type = e.target.dataset.type;
-        toggleShowSections();
+        console.log(type);
+        setWinner(types[type]);
     }
 });
+
+
+function setWinner(playerType){
+    toggleShowSections();
+    console.log(playerType);
+
+
+    const buttonPlayer = getElementByType(playerType.name);
+    
+    const houseOptionName = houseOption(playerType.name);
+    const buttonHouse = getElementByType(houseOptionName);
+    
+
+
+    optionPlayer.prepend(buttonPlayer);
+    optionHouse.prepend(buttonHouse);
+}
 
 
 function toggleShowSections(){
     board.classList.toggle("board--hidde");
     results.classList.toggle("result--hidde");
+}
+
+function getElementByType(type){
+    console.log(type);
+    const template = document.querySelector(`.${type}`);
+    console.log(template);
+    const content = template.content;
+
+    return content;
+}
+
+function houseOption(optionPlayer){
+
+    const currentOptions = options.filter((option) => option!==optionPlayer );
+    const getRandomNumber = Math.round(Math.random());
+    console.log(getRandomNumber);
+    return currentOptions[getRandomNumber];
 }
