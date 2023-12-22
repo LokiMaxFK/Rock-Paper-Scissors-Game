e11d98b5ed6a397035673a66a46a561472538fb3
Jordan <jordanacrga@gmail.com>
2023-12-21 18:24:07 -0600

    Ya se define el ganador

--- js/game.js
+++ js/game.js
@@ -16,13 +16,19 @@ const types = {
     }
 };
 
-const options = ["rock", "scissors", "paper"];
 
+let score = 0;
+const scoreElement = document.querySelector("[data-score]");
+const resetGame = document.querySelector("[data-id='reset-game']");
+const options = ["rock", "scissors", "paper"];
 const board = document.querySelector(".board");
 const results = document.querySelector(".result");
 const optionPlayer = document.querySelector(".result__option--player");
 const optionHouse = document.querySelector(".result__option--house");
+const resultFinally = document.querySelector(".result__finally");
 
+
+//Detectamos a que botón le dimos click
 board.addEventListener("click", (e)=>{
     console.log(e);
     if(e.target.matches(".option")){
@@ -32,31 +38,43 @@ board.addEventListener("click", (e)=>{
     }
 });
 
+//Reiniciamos el juego
+resetGame.addEventListener("click", ()=>{
+    toggleShowSections();
 
+});
+
+//Establecemos al ganador
 function setWinner(playerType){
     toggleShowSections();
-    console.log(playerType);
-
 
     const buttonPlayer = getElementByType(playerType.name);
-    
     const houseOptionName = houseOption(playerType.name);
-    const buttonHouse = getElementByType(houseOptionName);
-    
 
+
+    const buttonHouse = getElementByType(houseOptionName);
+    const houseType = types[houseOptionName];
 
     optionPlayer.prepend(buttonPlayer);
     optionHouse.prepend(buttonHouse);
+
+    if(playerType.beats === houseType.name){
+        resultFinally.textContent = "YOU WIN";
+        score++;
+        scoreElement.dataset.score = score;
+    }else{
+        resultFinally.textContent = "YOU LOOSE";
+    }
 }
 
-
+//Quitamos y mostramos a las secciones interactivas
 function toggleShowSections(){
     board.classList.toggle("board--hidde");
     results.classList.toggle("result--hidde");
 }
 
+//Extraemos el tipo de imagen que vamos a poner
 function getElementByType(type){
-    console.log(type);
     const template = document.querySelector(`.${type}`);
     console.log(template);
     const content = template.content;
@@ -64,10 +82,10 @@ function getElementByType(type){
     return content;
 }
 
+//Elegimos la opción que escogerá la casa
 function houseOption(optionPlayer){
-
     const currentOptions = options.filter((option) => option!==optionPlayer );
     const getRandomNumber = Math.round(Math.random());
-    console.log(getRandomNumber);
+
     return currentOptions[getRandomNumber];
 }
